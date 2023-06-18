d251d535674d24e4ab2b588f3571be3eb484c699
NavrA82 <124286244+NavrA82@users.noreply.github.com>
2023-06-18 23:37:39 +0300

    task-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,70 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const player = new Player('vimeo-player');
+const LOCALSTORAGE_KEY = 'videoplayer-current-time';
+
+player.on(
+  'timeupdate',
+  throttle(data => {
+    const LOCALSTORAGE_VALUE = data.seconds;
+    localStorage.setItem(LOCALSTORAGE_KEY, LOCALSTORAGE_VALUE);
+  }, 1000)
+);
+
+player
+  .setCurrentTime(localStorage.getItem(LOCALSTORAGE_KEY))
+  .then(function () {})
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+      default:
+        break;
+    }
+  });
+
+// варіант 2
+// function updatePosition(seconds) {
+//   localStorage.setItem(LOCALSTORAGE_KEY, seconds);
+// }
+
+// player.on(
+//   'timeupdate',
+//   throttle(data => updatePosition(data.seconds), 1000)
+// );
+
+// player
+//   .setCurrentTime(localStorage.getItem(LOCALSTORAGE_KEY))
+//   .then(function () {})
+//   .catch(function (error) {
+//     switch (error.name) {
+//       case 'RangeError':
+//         break;
+//       default:
+//         break;
+//     }
+//   });
+
+// варіант 1
+//   function updatePosition(seconds) {
+//     localStorage.setItem(LOCALSTORAGE_KEY, seconds);
+//   }
+
+//   const throttledUpdatePosition = throttle(updatePosition, 1000);
+
+//   player.on('timeupdate', function (data) {
+//     throttledUpdatePosition(data.seconds);
+//   });
+
+//   player
+//     .setCurrentTime(localStorage.getItem(LOCALSTORAGE_KEY))
+//     .then(function () {})
+//     .catch(function (error) {
+//       switch (error.name) {
+//         case 'RangeError':
+//           break;
+//         default:
+//           break;
+//       }
+//     });
